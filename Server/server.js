eee621d8292665b1b326cec4cbe7795882caafd9
Henry <henryugwu565@gmail.com>
2025-09-19 03:27:43 +0100

    Fix: Installed cors package implemting it to call my frontend

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoute.js';
+import cors from 'cors'
 
 // Making Necessary connections
 dotenv.config()
@@ -13,6 +14,10 @@ const app = express()
 const port = process.env.PORT || 5000
 
 //Middlewares
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 app.use(express.json())
 app.use(cookieParser())
 
