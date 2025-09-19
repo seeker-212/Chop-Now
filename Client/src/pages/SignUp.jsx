765dfda69c003aeec3e35276028d9fe55234fe58
Henry <henryugwu565@gmail.com>
2025-09-19 23:02:14 +0100

    Update: added google button for OAuth, made the form accept inputs

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { FaRegEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   // Creating the color variable
@@ -13,6 +15,12 @@ const SignUp = () => {
   //Use State Variables
   const [showPassword, setShowPassword] = useState(false);
   const [role, setRole] = useState("user");
+
+  //Registration State Variables
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div
@@ -40,10 +48,13 @@ const SignUp = () => {
             Full Name
           </label>
           <input
+            onChange={(e) => setFullName(e.target.value)}
+            value={fullName}
             type="text"
             className="w-full border border-gray-400  rounded-lg px-3 py-2 focus:outline-none 
             focus:border-green-500"
             placeholder="Enter your Full Name"
+            required
           />
         </div>
 
@@ -56,10 +67,13 @@ const SignUp = () => {
             Email
           </label>
           <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             type="text"
             className="w-full border border-gray-400  rounded-lg px-3 py-2 focus:outline-none 
             focus:border-green-500"
             placeholder="Enter your Email"
+            required
           />
         </div>
 
@@ -72,10 +86,13 @@ const SignUp = () => {
             Phone Number
           </label>
           <input
+            onChange={(e) => setMobile(e.target.value)}
+            value={mobile}
             type="tel"
             className="w-full border border-gray-400 rounded-lg px-3 py-2 focus:outline-none 
             focus:border-green-500"
             placeholder="Enter your Full Name"
+            required
           />
         </div>
 
@@ -89,10 +106,13 @@ const SignUp = () => {
           </label>
           <div className="relative ">
             <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               type={`${showPassword ? "text" : "password"}`}
               className="w-full border border-gray-400 rounded-lg px-3 py-2 focus:outline-none 
             focus:border-green-500"
               placeholder="Enter your Password"
+              required
             />
             <button
               className="absolute right-3 top-[13px] text-gray-500 cursor-pointer"
@@ -119,8 +139,8 @@ const SignUp = () => {
                 onClick={() => setRole(r)}
                 style={
                   role == r
-                    ? { backgroundColor: primaryColor, color: "white "}
-                    : { border: `1px solid ${borderColor}`, color: "#333" }
+                    ? { backgroundColor: primaryColor, color: "white " }
+                    : { border: `1px solid ${borderColor}`, color: "#32CD32" }
                 }
               >
                 {r}
@@ -128,9 +148,26 @@ const SignUp = () => {
             ))}
           </div>
         </div>
-        <button className={`w-full font-semibold py-2 rounded-lg transition duration-200 cursor-pointer
-        active:bg-white active:text-green-600 bg-[#32CD32] text-white`} 
-        >Sign Up</button>
+        <button
+          className={`w-full font-semibold py-2 rounded-lg transition duration-200 cursor-pointer
+        active:bg-white active:text-green-600 bg-[#32CD32] text-white`}
+        >
+          Sign Up
+        </button>
+
+        <button
+          className="w-full mt-4 flex items-center justify-center gap-2 border border-gray-400 rounded-lg
+        px-4 py-2 transition duration-200 hover:bg-gray-100 cursor-pointer"
+        >
+          <FcGoogle size={20} />
+          <span>Sign up with Google</span>
+        </button>
+        <p className="text-center mt-2">
+          Already have an account ?{" "}
+          <Link to="/signin" className="text-[#32CD32]">
+            Sign In
+          </Link>
+        </p>
       </div>
     </div>
   );
