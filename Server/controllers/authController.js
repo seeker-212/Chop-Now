63ee22dea35ec740183ee9c7d5d8372aface36d6
Henry <henryugwu565@gmail.com>
2025-09-19 02:55:25 +0100

    First commit: setup ChopNow project SignUp and SignIn implemented

---
+++ Server/controllers/authController.js
@@ -0,0 +1,90 @@
+import User from "../model/userModel.js";
+import bcrypt from 'bcryptjs'
+import genToken from "../utils/token.js";
+
+
+//Sign Up Conntroller for the User
+ export const signUp = async (req, res) => {
+    try {
+        //Getting the data from the form body
+        const {fullName, email, password, mobile, role} = req.body
+        const user = await User.findOne({email})
+
+        //Checking if User Already Exist 
+        if (user) {
+            return res.status(400).json({message: 'User already Exist.'})
+        }
+
+        //Making sure password is safe to use
+        if (password < 8) {
+            return res.status(400).json({message: 'Password must be at least 8 characters'})
+        }
+
+        //Making sure phone number is valid
+        if (mobile < 11) {
+            return res.status(400).json({message: ' Phone number must be at least 11 digits.'})
+        }
+
+        //------------HASING USER PASSWORD -------------
+        const hashedPassword = await bcrypt.hash(password, 12)
+
+        //Once everything is done then create User
+        user = await User.create({
+            fullName,
+            email,
+            password,
+            mobile,
+            password: hashedPassword
+        })
+
+        const token = await genToken(user._id)
+        res.cookies("token", token, {
+            secure: false,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 1000,
+            httpOnly: true
+        })
+
+        // After account is created
+        //Or is account is created successfully
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(`Sign up error ${error}`)
+    }
+}
+
+//Sign Up Conntroller for the User
+ export const signIn = async (req, res) => {
+    try {
+        //Getting the data from the form body
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+
+        //Checking if User does not Exist 
+        if (!user) {
+            return res.status(400).json({message: 'User does not exist.'})
+        }
+
+        //Checking if the gotten data is a match
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            res.status(400).json({message: "Incorrect Password"})
+        }
+
+        // Using the token to stay logged in
+        const token = await genToken(user._id)
+        res.cookies("token", token, {
+            secure: false,
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 1000,
+            httpOnly: true
+        })
+
+        //if login is successfully
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(`Sign In error ${error}`)
+    }
+}
---
+++ Server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import connectDB from './config/db.js';
+
+// Making Necessary connections
+dotenv.config()
+connectDB()
+
+//Creating the necessary Variables
+const app = express()
+const port = process.env.PORT || 8000
+
+app.use('/', (req, res) => (res.send('Api is working')))
+
+app.listen(port, (req, res) => (console.log(`Server is running on port:${port}`)))
