b0e018175d16d3f1669fa5c2c1c12fbd5979c0c8
Henry <henryugwu565@gmail.com>
2025-09-19 03:20:33 +0100

    Fix: updated the needed middlewares $ created and Api endpoint for auth

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './routes/authRoute.js';
 
 // Making Necessary connections
 dotenv.config()
@@ -9,6 +11,13 @@ connectDB()
 //Creating the necessary Variables
 const app = express()
 const port = process.env.PORT || 5000
+
+//Middlewares
+app.use(express.json())
+app.use(cookieParser())
+
+//Calling Endpoints
+app.use('/api/auth', authRouter)
 
 app.use('/', (req, res) => (res.send('Api is working')))
 
