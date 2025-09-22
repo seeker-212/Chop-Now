734920808638244e940f2c53c893d9420f424e45
Henry <henryugwu565@gmail.com>
2025-09-23 00:58:32 +0100

    Update: Created a navbar for the home page and it is still static tho

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,129 @@
+import React, { use, useState } from "react";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoIosSearch } from "react-icons/io";
+import { FiShoppingCart } from "react-icons/fi";
+import { useSelector } from "react-redux";
+import { RxCross2 } from "react-icons/rx";
+
+const Navbar = () => {
+  //Will get the user info from the UserSlice
+  const { userData } = useSelector((state) => state.user);
+
+  //Use State variable
+  const [showInfo, setShowInfo] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
+
+  return (
+    <div
+      className="w-full h-[80px] flex items-center justify-between md:justify-center gap-[30px]
+    px-[20px] fixed top-0 z-[9999] bg-[#fff9f6] overflow-visible"
+    >
+      {/* This Show search works for the small screen */}
+      {showSearch && (
+        <div
+          className="w-[90%] h-[70px] bg-white shadow-xl rounded-lg items-center gap-[20px]
+        flex fixed top-[80px] left-[5%] md:hidden"
+        >
+          <div
+            className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px]
+        border-gray-300"
+          >
+            <FaLocationDot size={25} className="text-[#32CD32]" />
+            <div className="w-[80%] truncate text-gray-600">Nnewi</div>
+          </div>
+          <div className="w-[80%] flex items-center gap-[10px]">
+            <IoIosSearch size={25} className="text-[#32CD32]" />
+            <input
+              type="text"
+              placeholder="Pick your cravings.."
+              className="text-gray-700 px-[10px] outline-0 w-full"
+            />
+          </div>
+        </div>
+      )}
+
+      {/* -----------This Website logo---------- */}
+      <h1 className="text-3xl font-bold mb-2 text-[#32CD32]">ChopNow</h1>
+
+      {/* This Search work for the bigger screen */}
+      <div
+        className="md:w-[60%] lg:w-[40%] h-[70px] bg-white shadow-xl rounded-lg items-center
+      gap-[20px] hidden md:flex"
+      >
+        <div
+          className="flex items-center w-[30%] overflow-hidden gap-[10px] px-[10px] border-r-[2px]
+        border-gray-300"
+        >
+          <FaLocationDot size={25} className="text-[#32CD32]" />
+          <div className="w-[80%] truncate text-gray-600">Nnewi</div>
+        </div>
+        <div className="w-[80%] flex items-center gap-[10px]">
+          <IoIosSearch size={25} className="text-[#32CD32]" />
+          <input
+            type="text"
+            placeholder="Pick your cravings.."
+            className="text-gray-700 px-[10px] outline-0 w-full"
+          />
+        </div>
+      </div>
+      <div className="flex gap-4 items-center">
+        {/* This search bar ony appear for small screen */}
+        {showSearch ? (
+          <RxCross2
+            size={25}
+            className="text-[#32CD32] md:hidden"
+            onClick={() => setShowSearch(false)}
+          />
+        ) : (
+          <IoIosSearch
+            size={25}
+            className="text-[#32CD32] md:hidden"
+            onClick={() => setShowSearch(true)}
+          />
+        )}
+
+        {/* This shopping cart section */}
+        <div className="relative cursor-pointer">
+          <FiShoppingCart size={25} className="text-[#32CD32]" />
+          <span className="absolute right-[-9px] top-[-12px] text-[#32CD32]">
+            0
+          </span>
+        </div>
+
+        {/* This my ORDERS section works for medium screen and above */}
+        <button
+          className="hidden md:block px-3 py-1 rounded-lg bg-[#32CD32]/10 text-[#32CD32]
+      cursor-pointer text-sm font-medium"
+        >
+          My Orders
+        </button>
+
+        {/* This is the user profile section */}
+        <div
+          onClick={() => setShowInfo((prev) => !prev)}
+          className="w-[40px] h-[40px] rounded-full flex items-center justify-center bg-[#32CD32]
+      text-white text-[15px] shadow-xl font-semibold cursor-pointer"
+        >
+          {userData.fullName.slice(0, 1)}
+        </div>
+        {showInfo && (
+          <div
+            className="fixed top-[80px] right-[10px] md:right-[10%] lg:right-[18%] w-[180px]
+        bg-white shadow-2xl rounded-xl p-[20px] flex flex-col gap-[10px] z-[9999]"
+          >
+            <div className="text-[17px] font-semibold">{userData.fullName}</div>
+            {/* This my ORDERS section works for small screen only */}
+            <div className="md:hidden text-[#32CD32] font-semibold cursor-pointer">
+              My Orders
+            </div>
+            <div className="text-[#32CD32] font-semibold cursor-pointer">
+              Log Out
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
