ea852641bed982d6c8ec7b7e119579630acc1054
Henry <henryugwu565@gmail.com>
2025-09-23 11:22:49 +0100

    Update: created a mongoose db model for my [Shop] and [Items], next: will working on the controllers

---
+++ Server/model/itemModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop", required: true },
+    category: {
+      type: String,
+      enum: [
+        "Snacks",
+        "Main Course",
+        "Desserts",
+        "Pizza",
+        "Burgers",
+        "Sandwiches",
+        "Eastern Naija",
+        "Southern Naija",
+        "chinese",
+        "Fast Food",
+        "Others",
+      ],
+      required: true,
+    },
+    price: { type: Number, min: 0, required: true },
+    foodType: { type: String, enum: ["veg", "non veg"], required: true },
+  },
+  { timestamps: true }
+);
+
+const Item = mongoose.model("Item", itemSchema);
+
+export default Item;
