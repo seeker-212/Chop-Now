362329e8d656b8ece79cc131b6a3bba783a0540b
Henry <henryugwu565@gmail.com>
2025-09-28 03:02:22 +0100

    Update: Added a rating section in my schema so user can rate my products

--- Server/model/itemModel.js
+++ Server/model/itemModel.js
@@ -24,6 +24,10 @@ const itemSchema = new mongoose.Schema(
     },
     price: { type: Number, min: 0, required: true },
     foodType: { type: String, enum: ["veg", "non veg"], required: true },
+    rating: {
+      average: { type: Number, default: 0 },
+      count: { type: Number, default: 0 },
+    },
   },
   { timestamps: true }
 );
