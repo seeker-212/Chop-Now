7f233b6098d69da59e48d1e1417221a548c00a04
Henry <henryugwu565@gmail.com>
2025-10-06 11:48:38 +0100

    Update: Create a route for the status update

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import isAuth from "../middleWares/isAuth.js";
-import { getMyOrders, placeOrder } from "../controllers/orderController.js";
+import {
+  getMyOrders,
+  placeOrder,
+  updateOrderStatus,
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place-order", isAuth, placeOrder);
 orderRouter.get("/my-orders", isAuth, getMyOrders);
-;
+orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus);
 
 export default orderRouter;
