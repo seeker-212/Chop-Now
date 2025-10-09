acf7019c59f5b09632ba18bcf2803fa96bd097c2
Henry <henryugwu565@gmail.com>
2025-10-09 04:07:40 +0100

    Update: Created a route for the controller

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -5,6 +5,7 @@ import {
   getCurrentOrder,
   getDeliveryAssignment,
   getMyOrders,
+  getOrderById,
   placeOrder,
   updateOrderStatus,
 } from "../controllers/orderController.js";
@@ -17,5 +18,6 @@ orderRouter.get("/get-assignment", isAuth, getDeliveryAssignment);
 orderRouter.get("/get-current-order", isAuth, getCurrentOrder);
 orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus);
 orderRouter.get("/accept-order/:assignmentId", isAuth, acceptOrder);
+orderRouter.get("/get-order-by-id/:orderId", isAuth, getOrderById);
 
 export default orderRouter;
