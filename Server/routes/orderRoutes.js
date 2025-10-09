1645e9e4c4745a4c2d7bf35400c04bbfabf0fe1d
Henry <henryugwu565@gmail.com>
2025-10-09 02:01:55 +0100

    Update : Route Done

--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import isAuth from "../middleWares/isAuth.js";
 import {
   acceptOrder,
+  getCurrentOrder,
   getDeliveryAssignment,
   getMyOrders,
   placeOrder,
@@ -13,6 +14,7 @@ const orderRouter = express.Router();
 orderRouter.post("/place-order", isAuth, placeOrder);
 orderRouter.get("/my-orders", isAuth, getMyOrders);
 orderRouter.get("/get-assignment", isAuth, getDeliveryAssignment);
+orderRouter.get("/get-current-order", isAuth, getCurrentOrder);
 orderRouter.post("/update-status/:orderId/:shopId", isAuth, updateOrderStatus);
 orderRouter.get("/accept-order/:assignmentId", isAuth, acceptOrder);
 
