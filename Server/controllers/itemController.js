0f95853327cd91ebeb35fd74c4ad8baba8bca516
Henry <henryugwu565@gmail.com>
2025-10-10 02:57:07 +0100

    Update: Created a controller that can get items by shop

--- Server/controllers/itemController.js
+++ Server/controllers/itemController.js
@@ -150,3 +150,22 @@ export const getItemByCity = async (req, res) => {
     return res.status(500).json({ message: `Error Getting by city Item ${error}` });
   }
 };
+
+
+export const getItemByShops = async (req, res) => {
+  try {
+    const {shopId} = req.params
+    const shop = await Shop.findById(shopId).populate('items')
+
+    if (!shop) {
+      return res.status(400).json({ message: "shops not found" });
+    }
+    
+    return res.status(200).json({
+      shop,
+      items: shop.items
+    })
+  } catch (error) {
+    return res.status(500).json({ message: `Error Getting item by shops ${error}` });
+  }
+}
