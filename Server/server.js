7a503bf6c9f73e4c4553475b1f8196216d53c33f
Henry <henryugwu565@gmail.com>
2025-10-13 10:15:17 +0100

    Update: implemmenting socket and making this necssary connections on my backend

--- Server/server.js
+++ Server/server.js
@@ -9,12 +9,27 @@ import userRouter from './routes/userRoutes.js';
 import shopRouter from './routes/shopRoute.js';
 import itemRouter from './routes/itemRoute.js';
 import orderRouter from './routes/orderRoutes.js';
+import http from 'http'
+import { Server } from 'socket.io';
 
 // Making Necessary connections
 connectDB()
 
 //Creating the necessary Variables
 const app = express()
+const server = http.createServer(app)
+
+//Implementing socket io
+const io = new Server(server, {
+    cors :{
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ['POST', 'GET']
+}
+})
+
+app.set('io', io)
+
 const port = process.env.PORT || 5000
 
 //Middlewares
@@ -36,4 +51,4 @@ app.use('/api/order', orderRouter)
 app.use('/', (req, res) => (res.send('Api is working')))
 
 //Listen for the active PORT
-app.listen(port, (req, res) => (console.log(`Server is running on port:${port}`)))
+server.listen(port, (req, res) => (console.log(`Server is running on port:${port}`)))
